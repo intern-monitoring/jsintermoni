ddcb879a4c05bf67e943a5856e2b3498f87e94a0
dimasardnt6 <dimasardnt6@gmail.com>
2023-10-30 19:37:01 +0700

    update dowpdown.js

--- components/dropdown.js
+++ components/dropdown.js
@@ -1,11 +1,11 @@
 const toggleProfileDropdown = () => {
-  const dropdown = document.querySelector(".dropdown-profile");
+  const dropdown = document.getElementById("profile-dropdown");
   dropdown.classList.toggle("opacity-100");
   dropdown.classList.toggle("hidden");
 };
 
 const toggleNotifikasiDropdown = () => {
-  const notifDropdown = document.querySelector(".notif-dropdown");
+  const notifDropdown = document.getElementById("notif-dropdown");
   notifDropdown.classList.toggle("opacity-100");
   notifDropdown.classList.toggle("hidden");
 };
