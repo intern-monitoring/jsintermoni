4da8651dd4cf0d4f1db5917152f51df1ec8c1142
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-02 15:32:19 +0700

    add deleteMagang.js

---
+++ deleteFunction/deleteMagang.js
@@ -0,0 +1,25 @@
+function deleteData(IDHAPUS) {
+  var magangId = IDHAPUS;
+  var target_url =
+    "https://asia-southeast2-bursakerja-project.cloudfunctions.net/intermoni-delete-magang?id=" +
+    magangId;
+
+  var requestOptions = {
+    method: "DELETE",
+    redirect: "follow",
+  };
+
+  fetch(target_url, requestOptions)
+    .then((response) => response.json())
+    .then(() => {
+      Swal.fire({
+        icon: "success",
+        title: "Data berhasil dihapus",
+        showConfirmButton: false,
+        timer: 1500,
+      }).then(() => {
+        location.reload();
+      });
+    })
+    .catch((error) => console.log("Error:", error));
+}
