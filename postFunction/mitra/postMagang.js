fe84ed00f8361611e53b5e886e66717ac5e1a8d3
dimasardnt6 <dimasardnt6@gmail.com>
2023-11-13 20:52:22 +0700

    Add postWithToken function for easier API calls

--- postFunction/mitra/postMagang.js
+++ postFunction/mitra/postMagang.js
@@ -1,6 +1,31 @@
-import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+function postWithToken(
+  target_url,
+  tokenkey,
+  tokenvalue,
+  datajson,
+  responseFunction
+) {
+  var myHeaders = new Headers();
+  myHeaders.append(tokenkey, tokenvalue);
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify(datajson);
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => responseFunction(JSON.parse(result)))
+    .catch((error) => console.log("error", error));
+}
 
 const PostMagang = () => {
   const target_url =
