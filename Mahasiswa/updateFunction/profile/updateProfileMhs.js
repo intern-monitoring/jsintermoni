9d310cdfad9b83affe46c6e39d953ad0840ffa37
dimasardnt6 <dimasardnt6@gmail.com>
2023-12-19 14:31:11 +0700

    Add file input event listener for image preview

--- Mahasiswa/updateFunction/profile/updateProfileMhs.js
+++ Mahasiswa/updateFunction/profile/updateProfileMhs.js
@@ -52,3 +52,22 @@ const updateProfileMhs = document.getElementById("updateProfileMhs");
 updateProfileMhs.addEventListener("click", () => {
   pushData(); // Call pushData function when the button is clicked
 });
+
+const handleFileInputChange = (event) => {
+  const input = event.target;
+
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const previewImage = document.getElementById("previewImage");
+      previewImage.src = e.target.result;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+};
+
+// Attach the event listener to the file input
+const imageInput = document.getElementById("imageInput");
+imageInput.addEventListener("change", handleFileInputChange);
